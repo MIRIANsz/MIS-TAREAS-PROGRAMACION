200c24fce4aeda220e95d24eb519210cc5a6e848
SEMANA 13 <miriansarango0@gmail.com>
2024-03-12 18:43:50 -0500

    Deberes

---
+++ Semana 11 - Biodimensional.py
@@ -0,0 +1,20 @@
+#Ejercicio
+
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+no_fila = 0
+busqueda = int(input('ingrese el valor de busqueda: '))
+
+for fila in matriz:
+    no_columna = 0
+    for columna in fila:
+        if (columna == busqueda):
+            print(f'la posicion es: [{no_fila}][{no_columna}] ')
+        no_columna += 1
+    no_fila += 1
+
+
