f3fc5b6905c2bd582fc5c4eb6bce54f5d71d5eef
SEMANA 13 <miriansarango0@gmail.com>
2024-03-25 09:41:35 -0500

    Deberes

--- my_notes.txt.py
+++ my_notes.txt.py
@@ -1,6 +1,7 @@
 # Escritura de archivo de texto
+import archivo
+
 with open("my_notes.txt", "w") as file:
-    # Escribir al menos tres líneas de notas personales
     file.write("Nota 1: Recordar comprar leche en el supermercado.\n")
     file.write("Nota 2: Llamar al médico para programar una cita.\n")
     file.write("Nota 3: Presentar la tarea de Fundamentos de Programación.\n")
@@ -14,6 +15,4 @@ with open("my_notes.txt", "r") as file:
         print(line.strip())  # Eliminar los saltos de línea adicionales
 
 # Cierre de archivo
-
-# No es necesario cerrar explícitamente el archivo al usar el contexto "with",
-# ya que se cierra automáticamente al salir del bloque "with".
+file.close()
